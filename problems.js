1202a470f6500fb3832a37f48615cd181a076acf
Cesar's PC <cesar261110@gmaill.com>
2022-11-02 20:45:47 -0700

    JS Practice complete

--- problems.js
+++ problems.js
@@ -144,6 +144,15 @@ function aCounter(word) {
     return count;
     */
     // Your code here
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        let char = word[i];
+        if (char === 'a' || char === 'A') {
+            count += 1;
+        }
+    }
+    return count
 };
 
 module.exports = {
